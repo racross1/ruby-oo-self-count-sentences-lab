af43585c48502ac8a8929118c295a239521df46a
racross1 <racross1@gmail.com>
2020-11-19 15:00:17 -0800

    I think my count_sentences is one I could have done better/cleaner

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    if self == " "
+      return 0
+    else
+      words_arr = self.split(" ")
+      new_words_arr = []
+      words_arr.each do |word|
+        if word.sentence? || word.question? || word.exclamation?
+          new_words_arr << word 
+        end 
+      end 
+    end 
+      new_words_arr.count
+    end 
 
-  end
+       
+ 
+ 
+
 end
